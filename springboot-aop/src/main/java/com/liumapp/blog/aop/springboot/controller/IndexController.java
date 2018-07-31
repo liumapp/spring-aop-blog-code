52c98cbe2b238d6c9f2364b67672440048e66910
liumapp <liumapp.com@gmail.com>
2018-07-31 20:29:59 +0800

    ready to test aop in controller

---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.liumapp.blog.aop.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file IndexController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/31
+ */
+@RestController
+@RequestMapping("")
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index () {
+        return "success";
+    }
+
+}
---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/monitor/ControllerMonitor.java
@@ -0,0 +1,11 @@
+package com.liumapp.blog.aop.springboot.monitor;
+
+/**
+ * @author liumapp
+ * @file ControllerMonitor.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/31
+ */
+public class ControllerMonitor {
+}
