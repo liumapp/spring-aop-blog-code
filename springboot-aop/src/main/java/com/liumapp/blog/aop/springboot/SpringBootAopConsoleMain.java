58c86afa85214bfe77b396c26247f363bd48c2e5
liumapp <liumapp.com@gmail.com>
2018-07-31 20:32:10 +0800

    rm unnecessary import

--- springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/SpringBootAopConsoleMain.java
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/SpringBootAopConsoleMain.java
@@ -2,7 +2,6 @@ package com.liumapp.blog.aop.springboot;
 
 import com.liumapp.blog.aop.springboot.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
