39b5fec50562934b848e9d6a28c931740e7fe085
liumapp <liumapp.com@gmail.com>
2018-07-31 20:07:11 +0800

    rm unnecessary things from springboot-aop

---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/SpringBootAopMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.blog.aop.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file SpringBootAopMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/31
+ */
+@SpringBootApplication
+public class SpringBootAopMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAopMain.class , args);
+    }
+
+}
