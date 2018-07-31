ece218cef7b52c96104f36379a5dd7925685262e
liumapp <liumapp.com@gmail.com>
2018-07-31 20:13:48 +0800

    add console main class and update yml

---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/SpringBootAopConsoleMain.java
@@ -0,0 +1,18 @@
+package com.liumapp.blog.aop.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file SpringBootAopConsoleMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/31
+ */
+@SpringBootApplication
+public class SpringBootAopConsoleMain {
+
+
+
+
+}
---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/service/HelloWorldService.java
@@ -0,0 +1,17 @@
+package com.liumapp.blog.aop.springboot.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liumapp
+ * @file HelloWorldService.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/31
+ */
+@Component
+public class HelloWorldService {
+
+
+
+}
