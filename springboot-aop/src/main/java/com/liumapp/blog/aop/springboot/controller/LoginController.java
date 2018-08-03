f1571dff92fd87311fa342865fed76a822662db1
liumapp <liumapp.com@gmail.com>
2018-08-03 14:45:33 +0800

    use aop to make login done

---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.liumapp.blog.aop.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file LoginController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/3
+ */
+@RestController
+@RequestMapping("login")
+public class LoginController {
+
+
+
+
+}
