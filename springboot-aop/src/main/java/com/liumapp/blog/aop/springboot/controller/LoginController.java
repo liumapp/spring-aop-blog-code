054cb2c4f37b4cb9509f580df79cc30c5a0c71a5
liumapp <liumapp.com@gmail.com>
2018-08-03 14:47:40 +0800

    add login monitor

--- springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/controller/LoginController.java
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/controller/LoginController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("login")
 public class LoginController {
 
-
-
+    
 
 }
---
+++ springboot-aop/src/main/java/com/liumapp/blog/aop/springboot/monitor/LoginMonitor.java
@@ -0,0 +1,4 @@
+package com.liumapp.blog.aop.springboot.monitor;
+
+public class LoginMonitor {
+}
