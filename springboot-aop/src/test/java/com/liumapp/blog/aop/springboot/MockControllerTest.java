9abc0f1e66465e53e245e953226b8563c6016006
liumapp <liumapp.com@gmail.com>
2018-08-06 11:14:42 +0800

    add mock mvc junit test

---
+++ springboot-aop/src/test/java/com/liumapp/blog/aop/springboot/MockControllerTest.java
@@ -0,0 +1,11 @@
+package com.liumapp.blog.aop.springboot;
+
+/**
+ * @author liumapp
+ * @file MockControllerTest.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/6
+ */
+public class MockControllerTest {
+}
